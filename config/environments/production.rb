509eb063d5984486efa1155b04d0594e6a20304c
Dylan Biddle <dylanbiddle@gmail.com>
2016-06-10 19:30:38 -0400

    Update mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'peaceful-reef-27215.herokuapp.com'
+  host = 'https://peaceful-reef-27215.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
