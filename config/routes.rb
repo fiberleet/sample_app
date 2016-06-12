f2bec62b5c759d94880f0075299de8e2602c2a92
Dylan Biddle <dylanbiddle@gmail.com>
2016-06-12 17:53:36 -0400

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#home'
+  root   'static_pages#home'
 
-  get 'help'    => 'static_pages#help'
-
-  get 'about'   => 'static_pages#about'
-
-  get 'contact' => 'static_pages#contact'
-
-  get 'signup'  => 'users#new'
-
-  get 'login'   => 'sessions#new'
-
-  post 'login'   => 'sessions#create'
+  get    'help'       => 'static_pages#help'
+      
+  get    'about'      => 'static_pages#about'
+      
+  get    'contact'    => 'static_pages#contact'
+      
+  get    'signup'     => 'users#new'
+      
+  get    'login'      => 'sessions#new'
+   
+  post   'login'     => 'sessions#create'
 
   delete 'logout'  => 'sessions#destroy'
 
@@ -21,7 +21,8 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
 
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  # The priority is based upon order of creation: first created -> highest priority.
+
+  resources :microposts,          only: [:create, :destroy]  # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
